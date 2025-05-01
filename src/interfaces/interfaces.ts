618ef199fa6215353f330066be9369caee5c2b93
Alexander <gamer34545@mail.ru>
2025-05-01 14:17:36 +0300

    add build

--- src/interfaces/interfaces.ts
+++
@@ -1,3 +0,0 @@
-export interface HighlightDecoratorProps {
-  children?: React.ReactNode;
-}
--- src/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type GenericStyle = Record<string, string | number>;
-
-export type Callback = (start: number, end: number) => void;
